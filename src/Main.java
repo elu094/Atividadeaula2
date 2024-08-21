3a0f844803cd8c5d36b008a4368e7c88f138bc89
EmerMarchi <marchimeson123@gmail.com>
2024-08-21 20:49:28 -0300

    If

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Gui !");
+    public static void main(String[] args){
+
+        Integer idadePessoa = 27;
+        String nomePessoa = "Emer";
+        if(idadePessoa > 21){
+            System.out.println("É maior que 21");
+        }else{
+            System.out.println("Não é maior");
+        }
     }
 }
