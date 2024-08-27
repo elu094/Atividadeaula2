3e474a50d25cf0a7b73bdc4255be5d213c8f7251
otaviobleich <ferreiraotavio577@gmail.com>
2024-08-27 09:45:51 -0300

    Criação do pojeto Java

---
+++ src/ExemploCondicao2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ExemploCondicao2 {
+    //Quero adicionar um desconto gradativo
+    //Valor maior que 1000 - 10%
+    //Valor maior que 2000 - 20%
+    //Valor maior que 3000 - 40%
+    //Valor menor igual a 1000 - 0%
+
+    public static void main(String[] args) {
+
+        double valorBruto = 3000;
+        double desconto = 0d;
+//Operadores Logicos
+// && e
+// || ou
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe o valor Bruto :");
+        valorBruto = scanner.nextDouble();
+        if(valorBruto > 1000 && valorBruto <= 2000) {
+             desconto = valorBruto * 0.10;
+        }
+        else if(valorBruto > 2000 && valorBruto < 3000) {
+             desconto = valorBruto * (20 / 100);
+        }
+        else if(valorBruto >= 3000) {
+             desconto = valorBruto * (30. / 100);
+        }
+
+        System.out.println("Valor Bruto é :" + valorBruto);
+        System.out.println("Valor do Descontop é :" + desconto);
+        System.out.println("Valor Liquido é :" + (valorBruto - desconto));
+    }
+}
